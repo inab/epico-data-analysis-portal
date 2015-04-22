c62cf8ebf788cc786d1286fabf3ee24f4bafbe80
José María Fernández <jmfernandez@cnio.es>
2015-04-22 22:30:50 +0200

    Fixed chromosome SVG files referencing

--- Gruntfile.js
+++ Gruntfile.js
@@ -239,7 +239,13 @@ module.exports = function (grunt) {
     usemin: {
       html: ['<%= yeoman.dist %>/{,*/}*.html'],
       css: ['<%= yeoman.dist %>/styles/{,*/}*.css'],
+      js: ['<%= yeoman.dist %>/scripts/{,*/}*.js'],
       options: {
+        patterns: {
+          js: [
+            [/(images\/Chromosome_[^.]+\.svg)/gm, 'Update the JS to reference our SVG images']
+          ]
+        },
         assetsDirs: ['<%= yeoman.dist %>','<%= yeoman.dist %>/images']
       }
     },

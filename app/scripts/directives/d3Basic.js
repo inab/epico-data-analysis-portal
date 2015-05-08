008c789e20f59e847d51c23e3a87f18384d88391
José María Fernández <jmfernandez@cnio.es>
2015-05-08 17:22:03 +0200

    Major overhaul on tree layout and data generation. Now, neither histones nor Cell Ontology terms are hard-coded

--- app/scripts/directives/d3Basic.js
+++ app/scripts/directives/d3Basic.js
@@ -104,15 +104,29 @@
 
           var svg = d3.select(iElement[0])
               .append("svg:svg")
-              .attr("width", "100%");  
-          var width, height, max;    
+              .attr("width", "100%");
+          
+          // These are global settings
+          var svgMargin = 20;
+          var expDataWidth = 60;
+          var sepFromTree = 145;
+          var expRadius = 15;
+          var clRadius = 10;
+          var expHeight = expRadius*2+10;
+          var expLabelWH = 150;
+          
+          // These are global variables
+          var width, height;    
           var tree = d3.layout.tree();    
           var root;
           var vis;
-          var i = 0;
+          var i;
           var diagonal = d3.svg.diagonal();
           var treew;
           var matirxw;
+          
+          var treeDepth;
+          var numNodes;
 
           // on window resize, re-render d3 canvas
           window.onresize = function() {
@@ -121,7 +135,7 @@
           scope.$watch(function(){
               return angular.element(window)[0].innerWidth;
             }, function(){
-              if(typeof(scope.data)=="object"){   
+              if(typeof(scope.data)==="object"){   
                 return scope.render(scope.data);
               }
             }
@@ -129,7 +143,7 @@
 
           // watch for data changes and re-render
           scope.$watch('data', function(newVals, oldVals) {
-            if(typeof(newVals)=="object"){ 
+            if(typeof(newVals)==="object"){ 
               return scope.render(newVals);
             }
           });
@@ -154,9 +168,10 @@
           };
 
           //define update function
-          scope.update =  function(source){
-
-            var duration = d3.event && d3.event.altKey ? 5000 : 500;
+          scope.update =  function(source,duration){
+		if(duration===undefined) {
+			duration = d3.event && d3.event.altKey ? 5000 : 500;
+		}
 
             // Compute the new tree layout.
             var nodes = tree.nodes(root).reverse();
@@ -166,36 +181,44 @@
                 .data(nodes, function(d) { return d.id  || (d.id = ++i); });
 
             // Normalize for treew/depth.
-            var ystep = treew/11;
-            nodes.forEach(function(d) {d.y = d.depth*ystep; d.x = height-d.id*40; });
+            var ystep = treew/treeDepth;
+            nodes.forEach(function(d) {d.y = d.depth*ystep; d.x = height-d.id*expHeight; });
 
             
 
             // Enter any new nodes at the parent's previous position.
             var nodeEnter = node.enter().append("svg:g")
-                .attr("class", "node")
-                .attr("transform", function(d) { return "translate(" + source.y0 + "," + source.x0 + ")"; })
-                .on("click", function(d) { scope.toggle(d); scope.update(d); });
+                .classed({node:true})
+                .attr("transform", "translate(" + source.y0 + "," + source.x0 + ")");
 
             //Display node horizontal lines
             nodeEnter.append("svg:line")
-                         .attr("x1", 0)
-                         .attr("y1", 0)
-                         .attr("x2", 2500)
-                         .attr("y2", 0)
+                         .attr("x1", function(d) { return d.children || d._children ? 30 : 30; })
+                         // .attr("y1", 0)
+                         .attr("y1", 20)
+                         .attr("x2", width-source.y0)
+                         // .attr("y2", 0)
+                         .attr("y2", 20)
                          .attr("stroke-width", 1)
-                         .attr("stroke", function(d){return (typeof(d.experimentsCount) != 'undefined')?"#EEE":  "#FFF";});
+                         .style("display",function(d){ return (typeof(d.experimentsCount) !== 'undefined')?"inherit":  "none";})
+                         .attr("stroke", function(d){return (typeof(d.experimentsCount) !== 'undefined')?"#EEE":  "#FFF";});
             
             nodeEnter.append("svg:circle")
                 .attr("r", 1e-6)
-                .style("fill", function(d) {return d.analized ? "rgb(232,0,0)" : ((typeof(d.experimentsCount) != 'undefined')?"#FFF":  "#CCC"); })
-                .style("stroke", function(d) {return d.analized ? "rgb(232,0,0)" : ((typeof(d.experimentsCount) != 'undefined')?"#4c00e2":  "#CCC"); });
+                .attr('title',function(d) { return d.name; })
+                .style("fill", function(d) {return d.analized ? "rgb(232,0,0)" : ((typeof(d.experimentsCount) !== 'undefined')?"#FFF":  "#CCC"); })
+                .style("stroke", function(d) {return d.analized ? "rgb(232,0,0)" : ((typeof(d.experimentsCount) !== 'undefined')?"#4c00e2":  "#CCC"); })
+		.on("click", function(d) { scope.toggle(d); scope.update(d); });
 
-            nodeEnter.append("svg:text")
+            nodeEnter.append("svg:a")
+		.attr("xlink:href",function(d) { return d.o_uri; })
+		.attr("target","_blank")
+		.append("svg:text")
                 .attr("x", function(d) { return d.children || d._children ? 30 : 30; })
-                .attr("y",-10)
+                // .attr("y",-10)
+                .attr("y",0)
                 .attr("dy", ".35em")
-                .attr("class", "label")
+                .classed({label:true})
                 .attr("text-anchor", function(d) { return d.children || d._children ? "start" : "start"; })
                 .text(function(d) { return d.name; })
                 .style("fill-opacity", 1e-6);
@@ -206,28 +229,31 @@
             //console.log(nodeEnter);
             //nodeData.forEach(function(v,i){
             //});
-              
+              // console.time("Jarl");
             nodeEnter.each(function(de){
                   
-                if (typeof de.expData != 'undefined') {
+                if (de.expData) {
 
 
                 var thisnode = d3.select(this);
-                var nodeTextData = de.expData; 
-                var nodeData = nodeTextData.split(",");
+                var nodeData = de.expData; 
                 
                 nodeData.forEach(function(v,i){
+			var theText = (!isNaN(v) && isFinite(v) && v !== -1)?(v==0?'0':(v<0.01?v.toExponential(1):v.toPrecision(2))):"--";
+			var theX = treew+sepFromTree-de.y-(svgMargin/2)+expDataWidth*i;
                     thisnode.append("svg:text")
-                    .attr("x", function(d) {return  treew+130-de.y+60*i; })
-                    .attr("y",-15)
+                    .attr("x", theX)
+                    .attr("y",0)
+                    // .attr("y",-15)
                     .attr("dy", ".35em")
-                    .attr("text-anchor", function(d) { return "start"; })
-                    .text(function(d) { return (v != -1 && v!='NaN' && v!='-Infinity')?v:"--";})
+                    .attr("text-anchor", "start")
+                    .text(theText)
                     .style("fill", "rgb(0,0,0)"); 
                     });
 
               }
             });  
+              // console.timeEnd("Jarl");
             
 
 
@@ -237,7 +263,7 @@
                 .attr("transform", function(d) {return "translate(" + d.y + "," + d.x + ")"; });
 
             nodeUpdate.select("circle")
-                .attr("r", function(d){return (d.experimentsCount && d.experimentsCount >0) ? 15: 10; });
+                .attr("r", function(d){ return (d.experimentsCount && d.experimentsCount >0) ? expRadius: clRadius; });
 
             nodeUpdate.select("text")
                 .style("fill-opacity", 1);
@@ -245,7 +271,7 @@
             // Transition exiting nodes to the parent's new position.
             var nodeExit = node.exit().transition()
                 .duration(duration)
-                .attr("transform", function(d) { return "translate(" + source.y + "," + source.x + ")"; })
+                .attr("transform", "translate(" + source.y + "," + source.x + ")")
                 .remove();
 
             nodeExit.select("circle")
@@ -260,7 +286,7 @@
 
             // Enter any new links at the parent's previous position.
             link.enter().insert("svg:path", "g")
-                .attr("class", "link")
+                .classed({link:true})
                 .attr("d", function(d) {
                   var o = {x: source.x0, y: source.y0};
                   return diagonal({source: o, target: o});
@@ -297,40 +323,83 @@
 
             //Draw headers
             if(data){
-
-              // setup variables
-              width = d3.select(iElement[0])[0][0].offsetWidth;
-              treew = width*0.27;
-              height = (scope.style == 'detailed')?6000:1840;
-              max = 98;
-              root = data.root;
-              var experiments = data.experiments;
-
+		// setup variables
+		var experiments = data.experiments;
+		root = data.root;
+		treeDepth = data.depth;
+		numNodes = data.numNodes;
+		// This has to be resetted with each tree
+		i=0;
+		
+		//treew = width*0.27;
+		treew = treeDepth*(expRadius*2)/3;
+		width = treew+sepFromTree+expDataWidth*experiments.length;
+		if(width < d3.select(iElement[0])[0][0].offsetWidth) {
+			width = d3.select(iElement[0])[0][0].offsetWidth;
+		}
+		
+		height = expLabelWH+numNodes*expHeight;
+		
+		//height = (scope.style === 'detailed')?6000:1840;
+              
+		var gExp = svg.selectAll("g.experiment")
+			.data(experiments)
+			.enter()
+			.append("svg:g")
+			.classed({experiment:true})
+			.attr("transform", function(d,i) { return "translate(" + (treew+sepFromTree+i*expDataWidth ) + "," + 100 + ")";});
+		
+		gExp.append("svg:foreignObject")
+			.attr("width",expLabelWH)
+			.attr("height","50")
+			.attr("transform","rotate(-70)")
+			.append("xhtml:body")
+			//.attr("style","background:transparent")
+			//.html(function() { return '<p>'+d+'</p>'; });
+			.style("background","transparent")
+			.append("xhtml:p")
+			.text(function(d) { return d; });
+		gExp.append("svg:line")
+			.attr("x1", 0)
+			.attr("y1", 10)
+			.attr("x2", 0)
+			.attr("y2", height-100)
+			.attr("stroke-width", 1)
+			.attr("stroke", "#EEE");
+			
+		// If we activate this, in Firefox it slows down a LOT due garbage collection
+		// gExp = undefined;
+		
+		/*	
               experiments.forEach(function(d,i){
                   svg.append("svg:g")
-                    .attr("transform", "translate(" + (treew+165+i*60 ) + "," + 100 + ")") 
+                    .attr("transform", "translate(" + (treew+sepFromTree+i*expDataWidth ) + "," + 100 + ")") 
                     .append("svg:foreignObject")
-                    .attr("width","150")
+                    .attr("width",expLabelWH)
                     .attr("height","50")
                     .attr("transform","rotate(-70)")
                     .append("xhtml:body")
-                    .attr("style","background:transparent")
-                    .html(function() { return '<p>'+d+'</p>'; });
+                    //.attr("style","background:transparent")
+                    //.html(function() { return '<p>'+d+'</p>'; });
+                    .style("background","transparent")
+                    .append("xhtml:p")
+                    .text(d);
 
 
                   //Display headers vertical lines
                   svg.append("svg:line")
-                           .attr("x1", treew+145+i*60)
+                           .attr("x1", treew+sepFromTree+i*expDataWidth)
                            .attr("y1", 200)
-                           .attr("x2", treew+145+i*60)
+                           .attr("x2", treew+sepFromTree+i*expDataWidth)
                            .attr("y2", height)
                            .attr("stroke-width", 1)
                            .attr("stroke", function(d){return "#EEE"; });  
               });
+		*/
 
 
               //margins
-              var m = [20, 20, 20, 20],
+              var m = [svgMargin, svgMargin, svgMargin, svgMargin],
               w = width - m[1] - m[3],
               h = height - m[0] - m[2];
 
@@ -340,14 +409,14 @@
               vis = svg.attr("width", w + m[1] + m[3])
                 .attr("height", h + m[0] + m[2])
                 .append("svg:g")
-                .attr("transform", "translate(" + m[3] + ",-200)");    
+                .attr("transform", "translate(" + m[3] + ","+m[0]+")");    
 
               diagonal.projection(function(d) { return [d.y, d.x]; });
 
               //create the graph
               tree.size([h, w]);
               tree.separation(function(a,b){
-                return a.parent == b.parent ? 1 : 2;
+                return (a.parent && b.parent && a.parent == b.parent) ? 1 : 2;
               });
 
               // Initialize the display to show a few nodes.
@@ -357,7 +426,7 @@
               // scope.toggle(root.children[9]);
               // scope.toggle(root.children[9].children[0]);  
 
-              scope.update(root);  
+              scope.update(root,0);  
 
             }
 

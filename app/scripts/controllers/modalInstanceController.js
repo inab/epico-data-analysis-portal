be905599b90564778b8dd48d4582c9ce20346bce
José María Fernández <jmfernandez@cnio.es>
2015-12-04 01:58:11 +0100

    Update to new ui.bootstrap class naming

--- app/scripts/controllers/modalInstanceController.js
+++ app/scripts/controllers/modalInstanceController.js
@@ -9,16 +9,16 @@
  * # ModalInstanceCtrl
  * Controller of the modal windows
  */
-angular.module('blueprintApp').controller('ModalInstanceCtrl', function ($scope, $modalInstance, modalState, modalMessage) {
+angular.module('blueprintApp').controller('ModalInstanceCtrl', function ($scope, $uibModalInstance, modalState, modalMessage) {
 	$scope.modalState = modalState;
 	$scope.modalMessage = modalMessage;
 	
 	$scope.okModal = function () {
 		//$modalInstance.close(someResult);
-		$modalInstance.close();
+		$uibModalInstance.close();
 	};
 	
 	$scope.cancelModal = function () {
-		$modalInstance.dismiss('cancel');
+		$uibModalInstance.dismiss('cancel');
 	};
 });

51829ae0d4752f4c93a421de0462f2ecba97eaaa
José María Fernández <jmfernandez@cnio.es>
2015-12-27 15:40:04 +0100

    Added programmatic color palette, based on jalette (https://github.com/emersion/jalette)

---
+++ app/scripts/services/color-palette.js
@@ -0,0 +1,126 @@
+'use strict';
+
+/* jshint camelcase: false , quotmark: false */
+/* globals jalette: false */
+
+/*
+ * We want to use an initial palette for Jalette
+ * so it does not take so much to get a new color
+ * This initial palette was borrowed from d3 category20
+ */
+var InitialPalette = [
+	'#1f77b4',
+	'#aec7e8',
+	'#ff7f0e',
+	'#ffbb78',
+	'#2ca02c',
+	'#98df8a',
+	'#d62728',
+	'#ff9896',
+	'#9467bd',
+	'#c5b0d5',
+	'#8c564b',
+	'#c49c94',
+	'#e377c2',
+	'#f7b6d2',
+	'#7f7f7f',
+	'#c7c7c7',
+	'#bcbd22',
+	'#dbdb8d',
+	'#17becf',
+	'#9edae5',
+];
+
+/**
+ * Creates a ColorPalette object
+ * @param	{Array}	initialPalette	An array with the initial palette colours, in RGB format
+ * @return	{ColorPalette}	A programmatic colour palette
+ */
+function ColorPalette(initialPalette) {
+	this.reset(initialPalette);
+}
+
+/**
+ * Resets the programmatic palette to an initial state
+ * @param	{Array}	initialPalette	An array with the initial palette colours, in RGB format
+ */
+ColorPalette.prototype.reset = function(initialPalette) {
+	this.palette = undefined;
+	
+	if(Array.isArray(initialPalette)) {
+		initialPalette = angular.copy(initialPalette);
+	} else {
+		initialPalette = InitialPalette;
+	}
+	
+	this.initialPalette = initialPalette;
+};
+
+/**
+ * Resets the programmatic palette, setting up the initial one
+ * @param	{Array}	initialPalette	An array with the initial palette colours, in RGB format
+ */
+ColorPalette.prototype.setInitialPalette = function(initialPalette) {
+	if(Array.isArray(initialPalette)) {
+		// A new palette implies starting from scratch
+		this.reset(initialPalette);
+	}
+};
+
+/**
+ * This method pre-computes up to numColors, it they were not already pre-computed
+ * @param	{Number}	numColors	The number of colours to pre-compute
+ */
+ColorPalette.prototype.calculateColors = function(numColors) {
+	if(this.palette === undefined) {
+		this.palette = this.initialPalette.map(function(rgbColor) {
+			return jalette.Rgb.fromHex(rgbColor);
+		});
+	}
+	
+	if(this.palette.length < numColors) {
+		for(var ipal=this.palette.length; ipal < numColors; ipal++) {
+			var lab = jalette.generateColor(this.palette);
+			this.palette.push(lab);
+		}
+	}
+};
+
+/**
+ * It returns an array of numColors colours, all of them different and divergent
+ * @param	{Number}	numColors	The number of colours to return
+ * @return	{Array}	An array with the first numColors colours from the programmatic palette, in RGB format
+ */
+ColorPalette.prototype.getColorArray = function(numColors) {
+	// First, assure the colors are in place
+	this.calculateColors(numColors);
+	
+	// Now, return the array, translated to RGB strings
+	return this.palette.map(function(color) {
+		return color.toRgb().toHex();
+	});
+};
+
+/**
+ * It returns the iColor-th colour from the programmatic palette
+ * @param	{Number}	iColor	The index of the colour to return
+ * @return	{String}	The colour, in RGB format
+ */
+ColorPalette.prototype.getColor = function(iColor) {
+	// First, assure the colors are in place
+	this.calculateColors(iColor+1);
+	
+	// Now, return the color, translated to RGB string
+	return this.palette[iColor].toRgb().toHex();
+};
+
+angular.
+module('blueprintApp').
+factory('ColorPalette', function () {
+	
+	return {
+		newInstance: function(initialPalette) {
+			return new ColorPalette(initialPalette);
+		},
+	};
+});

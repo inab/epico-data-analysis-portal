9d22d9a81c59188d2d652b1b6cffd36a08adaf34
José María Fernández <jmfernandez@cnio.es>
2016-03-01 16:29:46 +0100

    Added needed code to draw axes breaks on Highcharts.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -60,6 +60,44 @@ angular.module('blueprintApp', [
 	$tooltipProvider.setTriggers({
 		'never': 'mouseleave' // <- This ensures the tooltip will go away on mouseleave
 	});
+	
+	// This is to provide a graphical feedback on Highcharts y axes breaks
+	/**
+	 * Extend the Axis.getLinePath method in order to visualize breaks with two parallel
+	 * slanted lines. For each break, the slanted lines are inserted into the line path.
+	 */
+	try {
+		Highcharts.wrap(Highcharts.Axis.prototype, 'getLinePath', function (proceed, lineWidth) {
+			var axis = this,
+			path = proceed.call(this, lineWidth),
+			x = path[1],
+			y = path[2];
+			
+			Highcharts.each(this.breakArray || [], function (brk) {
+				if (axis.horiz) {
+					x = axis.toPixels(brk.from);
+					path.splice(3, 0,
+						'L', x - 4, y, // stop
+						'M', x - 9, y + 5, 'L', x + 1, y - 5, // left slanted line
+						'M', x - 1, y + 5, 'L', x + 9, y - 5, // higher slanted line
+						'M', x + 4, y
+					);
+				} else {
+					y = axis.toPixels(brk.from);
+					path.splice(3, 0,
+						'L', x, y - 4, // stop
+						'M', x + 5, y - 9, 'L', x - 5, y + 1, // lower slanted line
+						'M', x + 5, y - 1, 'L', x - 5, y + 9, // higher slanted line
+						'M', x, y + 4
+					);
+				}
+			});
+			return path;
+		});
+	} catch(err) {
+		console.log('Highcharts not available. Reason:');
+		console.log(err);
+	}
   });
 
 

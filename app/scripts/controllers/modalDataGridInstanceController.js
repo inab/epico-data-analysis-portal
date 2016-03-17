7d0724f3519fdaad0b263e2516e514003c7145c2
José María Fernández <jmfernandez@cnio.es>
2016-03-17 23:13:39 +0100

    Hyperlinks are now treated as such

--- app/scripts/controllers/modalDataGridInstanceController.js
+++ app/scripts/controllers/modalDataGridInstanceController.js
@@ -9,7 +9,7 @@
  * # ModalDataGridInstanceCtrl
  * Controller of the modal data grid windows
  */
-angular.module('blueprintApp').controller('ModalDataGridInstanceCtrl', function ($scope, $uibModalInstance, modalDataGrid) {
+angular.module('blueprintApp').controller('ModalDataGridInstanceCtrl', function ($scope, $sce, $uibModalInstance, modalDataGrid) {
 	$scope.modalDataGrid = modalDataGrid;
 	if(modalDataGrid.limit===undefined) {
 		modalDataGrid.limit = 50;
@@ -23,4 +23,15 @@ angular.module('blueprintApp').controller('ModalDataGridInstanceCtrl', function 
 	$scope.cancelModal = function () {
 		$uibModalInstance.dismiss('cancel');
 	};
+	
+	$scope.cellFormatter = function(text) {
+		var sanText = document.createTextNode(text);
+		var result = text;
+		if(typeof text === 'string' && (text.indexOf('http://')==0 || text.indexOf('https://')==0)) {
+			result = $sce.trustAsHtml('<a href="'+text+'" target="_blank">'+text+'</a>');
+		}
+		return result;
+		// 
+		// return (typeof text === 'string' && text.indexOf('http://')==0 || text.indexOf('https://')==0) ? $sce.trustAsUrl(text) : text;
+	}
 });

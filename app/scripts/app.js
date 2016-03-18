e93e01771bc1917596dbf8a414ae1f62f182ce74
José María Fernández <jmfernandez@cnio.es>
2016-03-18 02:00:02 +0100

    Fixed on axis breaks graphical representation when there is a breakSize

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -78,24 +78,53 @@ angular.module('blueprintApp', [
 			Highcharts.each(this.breakArray || [], function (brk) {
 				if (axis.horiz) {
 					x = axis.toPixels(brk.from);
+					var x2 = axis.toPixels(brk.to);
 					path.splice(3, 0,
 						'L', x - 4, y, // stop
 						'M', x - 9, y + 5, 'L', x + 1, y - 5, // left slanted line
-						'M', x - 1, y + 5, 'L', x + 9, y - 5, // higher slanted line
-						'M', x + 4, y
+						'M', x2 - 1, y + 5, 'L', x2 + 9, y - 5, // higher slanted line
+						'M', x2 + 4, y
 					);
 				} else {
 					y = axis.toPixels(brk.from);
+					var y2 = axis.toPixels(brk.to);
 					path.splice(3, 0,
 						'L', x, y - 4, // stop
 						'M', x + 5, y - 9, 'L', x - 5, y + 1, // lower slanted line
-						'M', x + 5, y - 1, 'L', x - 5, y + 9, // higher slanted line
-						'M', x, y + 4
+						'M', x + 5, y2 - 1, 'L', x - 5, y2 + 9, // higher slanted line
+						'M', x, y2 + 4
 					);
 				}
 			});
 			return path;
 		});
+		//    /**
+		//     * On top of each column, draw a zigzag line where the axis break is.
+		//     */
+		//    function pointBreakColumn(e) {
+		//	var point = e.point,
+		//	    brk = e.brk,
+		//	    shapeArgs = point.shapeArgs,
+		//	    x = shapeArgs.x,
+		//	    y = this.translate(brk.from, 0, 1, 0, 1),
+		//	    y2 = this.translate(brk.to, 0, 1, 0, 1),
+		//	    w = shapeArgs.width,
+		//	    key = ['brk', brk.from, brk.to],
+		//	    path = ['M', x, y, 'L', x + w * 0.25, y + 4, 'L', x + w * 0.75, y2 - 4, 'L', x + w, y2];
+                //
+		//	if (!point[key]) {
+		//	    point[key] = this.chart.renderer.path(path)
+		//		.attr({
+		//		    'stroke-width': 2,
+		//		    stroke: point.series.options.borderColor
+		//		})
+		//		.add(point.graphic.parentGroup);
+		//	} else {
+		//	    point[key].attr({
+		//		d: path
+		//	    });
+		//	}
+		//    }
 	} catch(err) {
 		console.log('Highcharts not available. Reason:');
 		console.log(err);

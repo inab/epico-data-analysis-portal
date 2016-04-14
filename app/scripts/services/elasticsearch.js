31f5b39dd9591c781e831880166bfab93d32f54c
José María Fernández <jmfernandez@cnio.es>
2016-04-14 19:59:08 +0200

    Elasticsearch requestTimeout has been set to a higher value.

--- app/scripts/services/elasticsearch.js
+++ app/scripts/services/elasticsearch.js
@@ -2,9 +2,12 @@
 
 angular.module('blueprintApp.elasticsearch',['blueprintApp.config'])
     .factory('es', function (esFactory,portalConfig) {
-  
+
+	var REQUEST_TIMEOUT = 300000;
+
       return esFactory({
-        host: portalConfig.esHost
+        host: portalConfig.esHost,
+        requestTimeout: REQUEST_TIMEOUT
         // ...
     });
 });
